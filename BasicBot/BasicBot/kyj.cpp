a06542b578b6365d3de9879016a7021798a3bc1f
kimyujin <calatheas@naver.com>
2017-06-24 14:42:40 +0900

    first commit

---
+++ BasicBot/BasicBot/kyj.cpp
@@ -0,0 +1,30 @@
+﻿//kyj.cpp
+#include "kyj.h"
+Kyj & Kyj::Instance()
+{
+	static Kyj instance;
+	return instance;
+}
+void Kyj::onSendText(std::string text){
+	if (text.find("speed ") == 0){
+		try{
+			int tmpSpeed = std::stoi(text.substr(text.find(" ") + 1));
+			BWAPI::Broodwar->setLocalSpeed(tmpSpeed);
+			std::cout << "setLocalSpeed=" << tmpSpeed << std::endl;
+		}
+		catch (std::exception &e){
+			std::cout << e.what() << std::endl;
+		}
+	}
+	//잘안먹음
+	else if (text.find("setFrameSkip ") == 0){
+		try{
+			int tmpSkip = std::stoi(text.substr(text.find(" ") + 1));
+			BWAPI::Broodwar->setFrameSkip(tmpSkip);
+			std::cout << "setFrameSkip=" << tmpSkip << std::endl;
+		}
+		catch (std::exception &e){
+			std::cout << e.what() << std::endl;
+		}
+	}
+}
---
+++ BasicBot/BasicBot/kyj.h
@@ -0,0 +1,9 @@
+﻿//kyj.h
+#pragma once
+#include "Common.h"
+#include <exception>
+class Kyj{
+public:
+	static Kyj & Instance();
+	void onSendText(std::string text);
+};
